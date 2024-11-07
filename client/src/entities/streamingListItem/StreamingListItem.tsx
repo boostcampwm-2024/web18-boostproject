a5fc226debf1181f64f3dd3a99d913e3d5092afc
Kang Chaeryeon <87600308+chaeryeon823@users.noreply.github.com>
2024-11-07 18:49:49 +0900

    feat: 앨범 스트리밍 api 구현

--- client/src/entities/streamingListItem/StreamingListItem.tsx
+++ client/src/entities/streamingListItem/StreamingListItem.tsx
@@ -1,8 +1,32 @@
-export function StreamingListItem() {
+interface StreamingListItemProps {
+  room: {
+    roomId: string;
+    album: string;
+    singer: string;
+    tags: string[];
+    streamingStatus: boolean;
+  };
+}
+
+export function StreamingListItem({ room }: StreamingListItemProps) {
+  // 최적화 필요
+  const tagString = `#${room.tags.join(' #')}`;
+  const enterRoomHandler = () => {
+    fetch('http://localhost:3000/rooms', {
+      method: 'POST',
+      body: JSON.stringify({ roomId: room.roomId }),
+    }).then((response) => {
+      if (response.ok) {
+        console.log(`Entered room ${room.roomId}`);
+      }
+    });
+  };
   return (
     <li>
-      <p className="text-grayscale-50">침착맨의 외힙</p>
-      <p className="text-grayscale-400 text-xs">#힙합 #침착맨 #외힙</p>
+      <div onClick={enterRoomHandler}>
+        <p className="text-grayscale-50 truncate">{room.album}</p>
+        <p className="text-grayscale-400 text-xs truncate">{tagString}</p>
+      </div>
     </li>
   );
 }
--- client/src/widgets/sidebar/StreamingList.tsx
+++ client/src/widgets/sidebar/StreamingList.tsx
@@ -1,10 +1,30 @@
 import { StreamingListItem } from '@/entities/streamingListItem';
 import './StreamingList.css';
+import { useEffect, useState } from 'react';
+
+interface RoomData {
+  roomId: string;
+  album: string;
+  singer: string;
+  tags: string[];
+  streamingStatus: boolean;
+}
+
 export default function StreamingList() {
+  const [roomList, setRoomList] = useState<RoomData[]>([]);
+
+  useEffect(() => {
+    fetch('/rooms')
+      .then((response) => response.json())
+      .then((data) => {
+        setRoomList(data.body);
+      });
+  }, []);
+
   return (
-    <ul className="flex flex-col gap-2 max-h-48 overflow-y-auto">
-      {Array.from({ length: 10 }).map((_, index) => (
-        <StreamingListItem key={index} />
+    <ul className="flex flex-col gap-2 max-h-48 overflow-y-auto pr-3">
+      {roomList.map((room) => (
+        <StreamingListItem key={room.roomId} room={room} />
       ))}
     </ul>
   );
