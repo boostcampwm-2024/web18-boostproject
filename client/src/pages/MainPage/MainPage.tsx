0d32d214090d3a7007e5526df096709cec9db49d
Kang Chaeryeon <87600308+chaeryeon823@users.noreply.github.com>
2024-11-10 18:53:59 +0900

    feat: 메인페이지 레이아웃 구현

--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -1,3 +1,26 @@
+import EventImage from '@/assets/main-evnet.png';
+import LogoAlbum from '@/assets/logo-album-cover.png';
 export function MainPage() {
-  return <div>메인페이지입니다</div>;
+  return (
+    <div className="p-8 pt-20">
+      <img src={EventImage} alt="스트리밍이 되고 있는 앨범 사진" />
+      <div className="text-gray-50">
+        <p className="mt-[70px] mb-7 text-3xl font-bold">최근 등록된 앨범</p>
+        <ul className="flex flex-row gap-9 justify-between	">
+          {Array.from({ length: 7 }).map((_, index) => (
+            <li key={index}>
+              <img
+                src={LogoAlbum}
+                alt="앨범 커버가 등록되지 않은 앨범"
+                className="mb-3"
+              />
+              <p className="text-xl font-semibold mb-1">앨범명명명</p>
+              <p className="mb-2">가수명명명</p>
+              <p className="text-gray-400 mb-1">#태그 #태그 #태그</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
