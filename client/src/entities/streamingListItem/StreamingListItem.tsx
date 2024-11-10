ee870b82321212553d574d8468c5eaefc24dc503
Kang Chaeryeon <87600308+chaeryeon823@users.noreply.github.com>
2024-11-10 22:44:50 +0900

    fix: StreamingListItem 깃 충돌 오류

---
+++ client/src/entities/streamingListItem/StreamingListItem.tsx
@@ -0,0 +1,32 @@
+interface StreamingListItemProps {
+  room: {
+    roomId: string;
+    album: string;
+    singer: string;
+    tags: string[];
+    streamingStatus: boolean;
+  };
+}
+
+export function StreamingListItem({ room }: StreamingListItemProps) {
+  // 최적화 필요
+  const tagString = `#${room.tags.join(' #')}`;
+  const enterRoomHandler = () => {
+    fetch('http://localhost:3000/room', {
+      method: 'POST',
+      body: JSON.stringify({ roomId: room.roomId }),
+    }).then((response) => {
+      if (response.ok) {
+        console.log(`Entered room ${room.roomId}`);
+      }
+    });
+  };
+  return (
+    <li>
+      <div onClick={enterRoomHandler} className="cursor-default">
+        <p className="text-grayscale-50 truncate">{room.album}</p>
+        <p className="text-grayscale-400 text-xs truncate">{tagString}</p>
+      </div>
+    </li>
+  );
+}
