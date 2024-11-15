0987aea78134156d7fea9f1b0e817965454b54a3
rdyjun <rdyjun00@gmail.com>
2024-11-15 22:22:18 +0900

    fix: 통신 방식 수정

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -3,6 +3,5 @@ import { io } from 'socket.io-client';
 const URL = 'http://inear.live/rooms';
 
 export const socket = io(URL, {
-  autoConnect: false,
-  transports: ['websocket'],
+  autoConnect: false
 });
