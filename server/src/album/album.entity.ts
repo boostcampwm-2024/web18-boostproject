20476ef714392fc2074e03766704871bf3195910
rdyjun <rdyjun00@gmail.com>
2024-11-21 01:16:40 +0900

    feat; Album 엔티티 내 아이디 확인을 위한 getId 메소드 추가

--- server/src/album/album.entity.ts
+++ server/src/album/album.entity.ts
@@ -3,8 +3,8 @@ import { AlbumDto } from '@/admin/dto/AlbumDto';
 
 @Entity()
 export class Album {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
   @Column({ type: 'varchar', length: 50, nullable: false })
   title: string;
@@ -13,23 +13,28 @@ export class Album {
   artist: string;
 
   @Column({ type: 'varchar', length: 30 })
-  tags?: string;
+  tags: string;
 
   @Column({ name: 'release_date', type: 'char', length: 16, nullable: false })
   releaseDate: string;
 
   @Column({ name: 'banner_url', type: 'varchar', length: 500 })
-  bannerUrl?: string;
+  bannerUrl: string;
 
   @Column({ name: 'jacket_url', type: 'varchar', length: 500 })
-  jacketUrl?: string;
+  jacketUrl: string;
 
-  constructor(albumDto: AlbumDto) {
+  constructor(albumDto?: AlbumDto) {
+    if (!albumDto) return;
     this.title = albumDto.title;
     this.artist = albumDto.artist;
     this.tags = albumDto.tags;
     this.releaseDate = albumDto.releaseDate;
     this.bannerUrl = albumDto.bannerUrl;
     this.jacketUrl = albumDto.jacketUrl;
+  }
+
+  public getId() {
+    return this.id;
   }
 }
