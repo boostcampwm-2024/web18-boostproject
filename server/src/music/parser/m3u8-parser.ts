c4ab3850cd5851ec03ec6cd76c7039f012357404
Kontae <koet0928@gmail.com>
2024-11-20 16:50:06 +0900

    feat: 변환되는 m3u8 파일에 ts파일 불러오는 라우터 경로 직접 지정
    ex)
    #EXTINF:1.996911,
    /api/music/RANDOM_AHH_ALBUM_ID/0/playlist44.ts

--- server/src/music/parser/m3u8-parser.ts
+++ server/src/music/parser/m3u8-parser.ts
@@ -2,27 +2,42 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class M3U8Parser {
-  parse(content: string, skipSegments: number): string {
+  parse(
+    content: string,
+    skipSegments: number,
+    albumId: string,
+    songIndex: string,
+  ): string {
     const lines = content.split('\n');
     let segmentCount = 0;
-    
-    return lines
-      .map(line => {
-        if (!line) return null;
-        
-        if (line.startsWith('#EXT-X-MEDIA-SEQUENCE')) {
-          return `#EXT-X-MEDIA-SEQUENCE:${skipSegments}`;
-        }
 
-        if (line.startsWith('#EXTINF') || (!line.startsWith('#') && line)) {
-          const shouldInclude = segmentCount >= skipSegments;
-          if (!line.startsWith('#')) segmentCount++;
-          return shouldInclude ? line : null;
-        }
+    return (
+      lines
+        .map((line) => {
+          if (!line) return null;
 
-        return line;
-      })
-      .filter((line): line is string => line !== null)
-      .join('\n') + '\n';
+          if (line.startsWith('#EXT-X-MEDIA-SEQUENCE')) {
+            return `#EXT-X-MEDIA-SEQUENCE:${skipSegments}`;
+          }
+
+          if (line.startsWith('#EXTINF') || (!line.startsWith('#') && line)) {
+            const shouldInclude = segmentCount >= skipSegments;
+            if (!line.startsWith('#')) {
+              const segmentNumber = line.match(/playlist(\d+)\.ts/)?.[1];
+              segmentCount++;
+
+              if (shouldInclude && segmentNumber) {
+                return `/api/music/${albumId}/${songIndex}/playlist${segmentNumber}.ts`;
+              }
+              return null;
+            }
+            return shouldInclude ? line : null;
+          }
+
+          return line;
+        })
+        .filter((line): line is string => line !== null)
+        .join('\n') + '\n'
+    );
   }
 }
