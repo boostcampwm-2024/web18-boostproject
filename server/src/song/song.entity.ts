3f0166bc8c6109f887b384d1e9e5f0a427f5bdfa
rdyjun <rdyjun00@gmail.com>
2024-11-21 01:15:41 +0900

    refactor: song 엔티티 및 저장용 dto 구조 수정

--- server/src/song/song.entity.ts
+++ server/src/song/song.entity.ts
@@ -1,31 +1,39 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-import { SongDto } from '@/admin/dto/SongDto';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { SongSaveDto } from '@/song/songSave.dto';
+import { Album } from '@/album/album.entity';
 
 @Entity()
 export class Song {
   @PrimaryGeneratedColumn()
   private id: number;
 
-  @Column({ name: 'album_id', type: 'int', nullable: false })
-  private albumId: number;
+  @ManyToOne(() => Album, { nullable: false })
+  @JoinColumn({ name: 'album_id' })
+  private albumId: string;
 
   @Column({ type: 'varchar', length: 50, nullable: false })
   private title: string;
 
   @Column({ name: 'track_number', type: 'int', nullable: false })
-  private trackNumber: number;
+  private trackNumber!: number;
 
   @Column({ type: 'text' })
   private lyrics: string;
 
   @Column({ type: 'varchar', length: 200, nullable: false })
-  private composer: string;
+  private composer!: string;
 
   @Column({ type: 'varchar', length: 200, nullable: false })
   private writer: string;
 
   @Column({ type: 'varchar', length: 200, nullable: false })
-  private instrument: string;
+  private instrument!: string;
 
   @Column({ name: 'sources', type: 'varchar', length: 50 })
   private source: string;
@@ -33,15 +41,16 @@ export class Song {
   @Column({ type: 'int', nullable: false })
   private duration: number;
 
-  constructor(songDto: SongDto) {
+  constructor(songDto?: SongSaveDto) {
+    if (!songDto) return;
     this.albumId = songDto.albumId;
     this.title = songDto.title;
     this.trackNumber = songDto.trackNumber;
     this.lyrics = songDto.lyrics;
-    this.composer = songDto.composer;
-    this.writer = songDto.writer;
-    this.instrument = songDto.instrument;
+    this.composer! = songDto.composer;
+    this.writer! = songDto.writer;
+    this.instrument! = songDto.instrument;
     this.source = songDto.source;
-    this.duration = songDto.duration;
+    this.duration! = songDto.duration;
   }
 }
