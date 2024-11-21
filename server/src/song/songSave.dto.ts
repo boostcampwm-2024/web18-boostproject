fa6016136950867c6cbdac98ca7f9f9f761dfdd8
rdyjun <rdyjun00@gmail.com>
2024-11-21 10:26:56 +0900

    feat: song save dto 내용 수정

--- server/src/song/songSave.dto.ts
+++ server/src/song/songSave.dto.ts
@@ -1,56 +1,28 @@
-import { IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { SongDto } from '@/admin/dto/SongDto';
 
-export class SongSaveDto {
+export class SongSaveDto extends SongDto {
   @IsString()
   @IsNotEmpty()
-  albumId: number;
-
-  @IsString()
-  @IsNotEmpty()
-  title: string;
+  albumId: string;
 
   @IsNumber()
-  @Min(1)
-  trackNumber: number;
-
-  @IsString()
-  lyrics?: string;
-
-  @IsString()
-  composer: string;
-
-  @IsString()
-  writer: string;
-
-  @IsString()
-  instrument: string;
-
-  @IsString()
-  source: string;
-
-  @IsString()
   @IsNotEmpty()
-  duration: string;
+  duration: number;
 
-  constructor(
-    albumId: number,
-    title: string,
-    trackNumber: number,
-    lyrics: string,
-    composer: string,
-    writer: string,
-    instrument: string,
-    source: string,
-    duration: string,
-  ) {
+  constructor({
+    albumId,
+    title,
+    trackNumber,
+    lyrics = null,
+    composer,
+    writer,
+    instrument,
+    source,
+    duration,
+  }) {
+    super(title, trackNumber, composer, writer, instrument, source, lyrics);
     this.albumId = albumId;
-    this.title = title;
-    this.trackNumber = trackNumber;
-    this.lyrics = lyrics;
-    this.composer = composer;
-    this.writer = writer;
-    this.instrument = instrument;
-    this.source = source;
     this.duration = duration;
   }
 }
