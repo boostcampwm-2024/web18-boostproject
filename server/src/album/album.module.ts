bc68462b2b57b141b29115427ba6628d8cf84099
Kontae <koet0928@gmail.com>
2024-11-24 22:21:20 +0900

    feat: 메인 배너 feature 모듈에 지정

--- server/src/album/album.module.ts
+++ server/src/album/album.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Album } from '@/album/album.entity';
+import { AlbumController } from './album.controller';
+import { AlbumService } from './album.service';
+import { AlbumRepository } from './album.repository';
+import { AlbumRedisRepository } from './album.redis.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Album])],
-  controllers: [],
-  providers: [],
-  exports: [],
+  controllers: [AlbumController],
+  providers: [AlbumService, AlbumRepository, AlbumRedisRepository],
 })
 export class AlbumModule {}
