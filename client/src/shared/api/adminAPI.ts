8916ef6b2a70c91d243405bc3a0077a5f7eac28b
Kang Chaeryeon <87600308+chaeryeon823@users.noreply.github.com>
2024-11-26 23:57:36 +0900

    fix: adminAPI 충돌 수정

--- client/src/shared/api/adminAPI.ts
+++ client/src/shared/api/adminAPI.ts
@@ -1,22 +1,13 @@
 import axios from 'axios';
 
-const adminInstance = axios.create({
+const adminApi = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api`,
-});
-
-adminInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('authorization');
-  if (token) {
-    config.headers.Authorization = token.startsWith('Bearer ')
-      ? token
-      : `Bearer ${token}`;
-  }
-  return config;
+  withCredentials: true,
 });
 
 export const albumAPI = {
   createAlbum: async (formData: FormData) => {
-    return adminInstance.post('/admin/album', formData, {
+    return adminApi.post('/admin/album', formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
       },
