c9cee486f7f301c09d1a18f4ca8142339aa75c23
Yoonseo Han <51229971+yoonseo-han@users.noreply.github.com>
2024-11-26 14:27:40 +0800

    로컬 스토리지에서 쿠키로 로그인 로직 변경 (#117)
    * fix: gitignore 수정
    
    * feat: 관리자 로그인 페이지 만들기
    
    * feat: 로그인 기능 구현
    
    * feat: guard를 통한 어드민 엔드포인트 보호
    
    * feat: 평문 비교가 아닌 해시로 비교 하도록 변경
    
    * feat: ProtectedRoutes로 라우트 접근 보호 컴포넌트 추가
    
    * fix: Bearer 토큰에 관한 설정 진행
    
    * feat: axios interceptor로 어드민 api 요청에 관한 액세스에 토큰 추가 기능 구현
    
    * feat: 서버에서 쿠키 생성 로직 추가
    
    * feat: admin guard에 쿠키 파싱 진행
    
    * feat: 클라이언트에서 쿠키를 이용한 로그인 방법으로 리팩토링 진행

--- server/src/admin/admin.controller.ts
+++ server/src/admin/admin.controller.ts
@@ -1,7 +1,9 @@
 import {
   Body,
   Controller,
+  Get,
   Post,
+  Res,
   UploadedFiles,
   UseGuards,
   UseInterceptors,
@@ -18,6 +20,8 @@ import { RoomService } from '@/room/room.service';
 import { AdminGuard } from './admin.guard';
 import { plainToInstance } from 'class-transformer';
 import { MissingSongFiles } from '@/common/exceptions/domain/song/missing-song-files.exception';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 export interface UploadedFiles {
   albumCover?: Express.Multer.File;
@@ -28,6 +32,7 @@ export interface UploadedFiles {
 @Controller('admin')
 export class AdminController {
   constructor(
+    private configService: ConfigService,
     private readonly adminService: AdminService,
     private readonly musicProcessingService: MusicProcessingSevice,
     private readonly albumRepository: AlbumRepository,
@@ -35,12 +40,24 @@ export class AdminController {
   ) {}
 
   @Post('login')
-  async login(@Body() body: { adminKey: string }) {
-    return this.adminService.login(body.adminKey);
+  async login(
+    @Body() body: { adminKey: string },
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const result = await this.adminService.login(body.adminKey);
+
+    response.cookie('admin_token', result.token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      maxAge: parseInt(this.configService.get('TOKEN_EXPIRATION')) * 1000,
+    });
+
+    return { message: 'Login successful' };
   }
 
   @UseGuards(AdminGuard)
-  @Post('verify-token')
+  @Get('verify-token')
   async verifyAdminToken() {
     return { valid: true };
   }
