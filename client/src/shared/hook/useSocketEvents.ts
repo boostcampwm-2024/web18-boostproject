18c8a73b739f07ddc96a82d7b960e469d000a6e2
Kang Chaeryeon <87600308+chaeryeon823@users.noreply.github.com>
2024-11-26 14:55:57 +0900

    fix: 특수문자 채팅 다음줄로 넘어가게 수정

--- client/src/shared/hook/useSocketEvents.ts
+++ client/src/shared/hook/useSocketEvents.ts
@@ -10,6 +10,12 @@ interface UseSocketEventsProps {
   events: SocketEvents;
 }
 
+/**
+ * [현재 미사용]
+ * 소켓 연결과 스트리밍 전체 연결 확인 후에 삭제할 예정
+ * @returns
+ */
+
 export function useSocketEvents({ socket, events }: UseSocketEventsProps) {
   const registerEvents = useCallback(() => {
     Object.entries(events).forEach(([event, handler]) => {
