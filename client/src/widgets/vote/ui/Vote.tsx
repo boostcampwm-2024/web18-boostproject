17f1c0fb18116a54c23c12cdeaefb0758e888a9b
rdyjun <rdyjun00@gmail.com>
2024-11-27 22:00:19 +0900

    feat: voteUpdated 이벤트에 따라 출력 및 클릭 시 vote 이벤트 발생 구현

--- client/src/widgets/vote/ui/Vote.tsx
+++ client/src/widgets/vote/ui/Vote.tsx
@@ -1,8 +1,20 @@
 import { ChevronDown } from '@/shared/icon/ChevronDown';
 import { useState } from 'react';
+import { useSocketStore } from '@/shared/store/useSocketStore.ts';
+import { SongData } from '@/entities/album/types.ts';
+import { useVote } from '@/widgets/vote/useVote.ts';
+import './ScrollBar.css';
 
-export function Vote() {
+export function Vote({ songs }: { songs: SongData[] }) {
   const [isOpen, setIsOpen] = useState(false);
+  const { voteData } = useVote();
+  const { socket } = useSocketStore();
+
+  const handleVoteClick = (trackNumber: string) => {
+    if (!socket) return;
+    socket.emit('vote', { trackNumber });
+  };
+
   return (
     <div className="absolute top-[5.5rem] left-0 right-0 bg-grayscale-700  z-10 mx-8 rounded-lg">
       <div
@@ -11,7 +23,7 @@ export function Vote() {
       >
         <div className="mr-2">
           <p className="text-grayscale-300">최애의 트랙</p>
-          <p className="text-lg font-semibold">겨울 간식 뭐가 더 좋나</p>
+          <p className="text-lg font-semibold">내 취향 음악은?</p>
         </div>
         <div
           className={`transform transition-transform duration-200 ${isOpen ? 'rotate-180' : ''}`}
@@ -20,15 +32,33 @@ export function Vote() {
         </div>
       </div>
       <div
-        className={`overflow-hidden transition-all duration-200 ${isOpen ? 'max-h-40' : 'max-h-0'} px-4`}
+        className={`overflow-y-scroll transition-all duration-200 ${isOpen ? 'max-h-40' : 'max-h-0'} px-4`}
       >
-        {['슈크림 붕어빵', '팥 붕어빵', '피자 붕어빵'].map((item, index) => (
-          <p
-            key={index}
-            className="mb-1 cursor-pointer px-3 py-3 rounded-md hover:bg-grayscale-600"
-          >
-            {item}
-          </p>
+        {Object.values(voteData).map((item, index) => (
+          <div className="vote relative h-full my-3 flex w-full justify-between overflow-x-hidden hover:bg-grayscale-600 rounded-lg cursor-pointer">
+            <div>
+              <div
+                className={`votebg absolute bg-grayscale-800 text-grayscale-300 text-shadow h-full rounded-md z-0`}
+                style={{ width: `${item}` }}
+              ></div>
+              <p
+                key={index}
+                className={`relative mb-1 cursor-pointer px-3 py-3 rounded-md z-10`}
+                onClick={() => handleVoteClick(String(index + 1))}
+              >
+                {songs[index].title}
+              </p>
+            </div>
+            <div className={'w-10 w-20'}>
+              <p
+                key={index}
+                className={`mb-1 cursor-pointer px-3 py-3 rounded-md z-10`}
+                onClick={() => handleVoteClick(String(index + 1))}
+              >
+                {item}
+              </p>
+            </div>
+          </div>
         ))}
       </div>
     </div>
