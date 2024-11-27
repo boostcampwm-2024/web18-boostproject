5e28ba4eed698541d7b206b17a40f4b9af3613b6
Kontae <koet0928@gmail.com>
2024-11-27 22:09:17 +0900

    feat: 앨범 상세 댓글 불러오는 응답 dto 추가

---
+++ server/src/comment/dto/album-comment-response.dto.ts
@@ -0,0 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class AlbumCommentResponseDto {
+  @ApiProperty({ type: () => AlbumCommentDto, isArray: true })
+  result: {
+    albumComments: AlbumCommentDto[];
+  };
+  constructor(albumComments: AlbumCommentDto[]) {
+    this.result = {
+      albumComments,
+    };
+  }
+}
+
+export class AlbumCommentDto {
+  @ApiProperty()
+  albumId: string;
+  @ApiProperty()
+  content: string;
+}
