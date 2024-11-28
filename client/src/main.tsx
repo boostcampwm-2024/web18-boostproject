80cbc09e101130ee593a69e4a77216d35f443457
Kang Chaeryeon <87600308+chaeryeon823@users.noreply.github.com>
2024-11-28 20:40:45 +0900

    feat: 사이드바 앨범 리스트 query로 변경

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,19 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { Router } from '@/app/router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
-  <>
+  <QueryClientProvider client={queryClient}>
     <Router />
-  </>,
+  </QueryClientProvider>,
 );
---
+++ client/src/widgets/sidebar/hook/useSidebarAlbum.ts
@@ -0,0 +1,11 @@
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { publicAPI } from '@/shared/api/publicAPI';
+export const useSidebarAlbum = () => {
+  return useSuspenseQuery({
+    queryKey: ['albumSidebar'],
+    queryFn: async () => {
+      const response = await publicAPI.getAlbumSidebar();
+      return response.result;
+    },
+  });
+};
