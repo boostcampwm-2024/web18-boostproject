13fad4af64a14a6753e8945b87d877bfa6138643
rdyjun <rdyjun00@gmail.com>
2024-11-28 17:53:38 +0900

    feat: broadcast 이벤트 발생 시 사용자 id를 받아 출력하도록 수정 (#162)

--- client/src/widgets/chatting/ui/Message.tsx
+++ client/src/widgets/chatting/ui/Message.tsx
@@ -1,13 +1,17 @@
 interface MessageData {
   userName: string;
   message: string;
+  userId: string;
 }
 
-export function Message({ userName, message }: MessageData) {
+export function Message({ userName, message, userId }: MessageData) {
   return (
     <div className="text-sm pb-4">
-      <span className="text-brand mr-4 whitespace-nowrap">{userName}</span>
-      <span className="text-grayscale-100 break-words">{message}</span>
+      <span className="text-brand mr-1 whitespace-nowrap">{userName}</span>
+      <span className="text-grayscale-400 mr-4 whitespace-nowrap">
+        #{userId}
+      </span>
+      <span className="text-grayscale-100 break-words text-[2]">{message}</span>
     </div>
   );
 }
