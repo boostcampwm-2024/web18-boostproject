8c132541db08e1cff7932d49588b27f6461b69d2
Kang Chaeryeon <87600308+chaeryeon823@users.noreply.github.com>
2024-11-29 15:49:19 +0900

    hotfix: 메인 앨범 리스트 7개로 조정

--- client/src/widgets/albums/ui/AlbumCard.tsx
+++ client/src/widgets/albums/ui/AlbumCard.tsx
@@ -1,16 +1,25 @@
-import { Album } from '@/entities/album/types';
+interface AlbumCard {
+  albumId: string;
+  albumName: string;
+  artist: string;
+  albumTags?: string;
+  jacketUrl: string;
+}
 
-export function AlbumCard({ album }: { album: Album }) {
+export function AlbumCard({ album }: { album: AlbumCard }) {
+  const tagString = album.albumTags
+    ? `#${album.albumTags.split(', ').join(' #')}`
+    : '태그 없음';
   return (
     <li>
       <img
-        src={album.coverImage}
-        alt={`${album.title} 앨범 커버`}
+        src={album.jacketUrl}
+        alt={`${album.albumName} 앨범 커버`}
         className="mb-3 w-[200px] h-[200px] object-cover"
       />
-      <p className="text-xl font-semibold mb-1">{album.title}</p>
+      <p className="text-xl font-semibold mb-1 truncate">{album.albumName}</p>
       <p className="mb-2">{album.artist}</p>
-      <p className="text-grayscale-400 mb-1">#{album.tags.join(' #')}</p>
+      <p className="text-grayscale-400 mb-1">{tagString}</p>
     </li>
   );
 }
