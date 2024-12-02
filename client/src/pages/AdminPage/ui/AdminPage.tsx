ea2b47a33e7d72d8669fa6bc535ed9f2382d8f91
Kontae <koet0928@gmail.com>
2024-12-02 21:48:16 +0900

    feat: 관리자 페이지 홈 로고 추가

--- client/src/pages/AdminPage/ui/AdminPage.tsx
+++ client/src/pages/AdminPage/ui/AdminPage.tsx
@@ -2,6 +2,8 @@ import { Button } from '@/shared/ui';
 import { AlbumForm, SongForm } from '@/features/albumRegister';
 import { useAlbumForm } from '@/features/albumRegister/model/useAlbumForm';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { InearLogo } from '@/shared/icon/InearLogo';
 
 export function AdminPage() {
   const { handleSubmit, handleAddSong, songs, songFormRef, albumFormRef } =
@@ -29,32 +31,37 @@ export function AdminPage() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="w-full h-fit max-w-5xl grid grid-cols-6 gap-6">
-        <AlbumForm albumFormRef={albumFormRef} />
-        <SongForm songFormRef={songFormRef} />
-        <div className="col-span-2 flex flex-col gap-4">
-          <label className="text-grayscale-100 text-sm">목록</label>
-          <ul className="h-full text-grayscale-100 overflow-y-auto">
-            {songs.map((song, index) => (
-              <li key={index} className="py-2 border-b border-gray-200">
-                {index + 1}. {song.title}
-              </li>
-            ))}
-          </ul>
-          <div className="flex flex-row gap-4">
-            <Button message="노래 추가" onClick={handleAddSong} />
-            <Button
-              message={isLoading ? '생성 중...' : '방 만들기'}
-              onClick={handlePost}
-              disabled={isLoading}
-              style={{ opacity: isLoading ? 0.5 : 1 }}
-            />
-            {isSuccess && (
-              <span className="text-sm text-green-500 ml-2">
-                ✔ 방 생성 완료
-              </span>
-            )}
+    <div>
+      <Link to="/" className="block m-8 w-fit">
+        <InearLogo />
+      </Link>
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="w-full h-fit max-w-5xl grid grid-cols-6 gap-6">
+          <AlbumForm albumFormRef={albumFormRef} />
+          <SongForm songFormRef={songFormRef} />
+          <div className="col-span-2 flex flex-col gap-4">
+            <label className="text-grayscale-100 text-sm">목록</label>
+            <ul className="h-full text-grayscale-100 overflow-y-auto">
+              {songs.map((song, index) => (
+                <li key={index} className="py-2 border-b border-gray-200">
+                  {index + 1}. {song.title}
+                </li>
+              ))}
+            </ul>
+            <div className="flex flex-row gap-4">
+              <Button message="노래 추가" onClick={handleAddSong} />
+              <Button
+                message={isLoading ? '생성 중...' : '방 만들기'}
+                onClick={handlePost}
+                disabled={isLoading}
+                style={{ opacity: isLoading ? 0.5 : 1 }}
+              />
+              {isSuccess && (
+                <span className="text-sm text-green-500 ml-2">
+                  ✔ 방 생성 완료
+                </span>
+              )}
+            </div>
           </div>
         </div>
       </div>
