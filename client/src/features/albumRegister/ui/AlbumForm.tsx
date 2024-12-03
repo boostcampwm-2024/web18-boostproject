b656564b2c1ddf3985a590f98769c20213eda09f
Kang Chaeryeon <87600308+chaeryeon823@users.noreply.github.com>
2024-12-03 14:59:21 +0900

    feat: 관리자 페이지에 라벨로 제한 글자수 표시

--- client/src/features/albumRegister/ui/AlbumForm.tsx
+++ client/src/features/albumRegister/ui/AlbumForm.tsx
@@ -5,10 +5,10 @@ interface AlbumFormProps {
 export function AlbumForm({ albumFormRef }: AlbumFormProps) {
   return (
     <form ref={albumFormRef} className="col-span-2 flex flex-col gap-4">
-      <Input labelName="앨범 이름" name="title" />
-      <Input labelName="앨범 아티스트" name="artist" />
+      <Input labelName="앨범 이름 (50자 제한)" name="title" />
+      <Input labelName="앨범 아티스트 (50자 제한)" name="artist" />
       <Input
-        labelName="앨범 태그"
+        labelName="앨범 태그 (30자 제한, 쉼표와 공백 포함)"
         name="albumTag"
         placeholder="힙합, 댄스, 국내"
       />
--- client/src/features/albumRegister/ui/SongForm.tsx
+++ client/src/features/albumRegister/ui/SongForm.tsx
@@ -8,7 +8,7 @@ export function SongForm({ songFormRef }: SongFormProps) {
   return (
     <form ref={songFormRef} className="col-span-2 grid grid-cols-2 gap-4">
       <div className="col-span-2">
-        <Input labelName="노래 제목" name="title" />
+        <Input labelName="노래 제목 (30자 제한)" name="title" />
       </div>
       <div className="flex flex-col gap-4">
         <Input labelName="작곡가" name="composer" />
@@ -18,7 +18,7 @@ export function SongForm({ songFormRef }: SongFormProps) {
       <div className="flex flex-col gap-4">
         <Input labelName="작사가" name="writer" />
         <Input labelName="연주/가수" name="instrument" />
-        <Input labelName="출처" name="source" />
+        <Input labelName="출처 (50자 제한)" name="source" />
       </div>
       <div className="col-span-2">
         <label className="text-grayscale-100 text-sm">가사</label>
