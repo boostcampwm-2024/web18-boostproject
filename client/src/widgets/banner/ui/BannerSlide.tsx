758824941c8d0fc6ba98b84c7f3aceb5448f2c7a
Kang Chaeryeon <87600308+chaeryeon823@users.noreply.github.com>
2024-12-05 21:41:37 +0900

    hotfix: 배너 라이브 수정

--- client/src/widgets/banner/ui/BannerSlide.tsx
+++ client/src/widgets/banner/ui/BannerSlide.tsx
@@ -2,7 +2,7 @@ import { bannerData } from '@/entities/room/types';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { useNavigate } from 'react-router-dom';
-import { convertToKTC, splitDate } from '@/shared/util/timeUtils';
+import { compareDate, convertToKTC, splitDate } from '@/shared/util/timeUtils';
 
 interface BannerSlideProps {
   banner: bannerData;
@@ -10,12 +10,11 @@ interface BannerSlideProps {
 
 export function BannerSlide({ banner }: BannerSlideProps) {
   const navigate = useNavigate();
-
   const handleClick = () => {
     if (!banner.albumId) return;
     navigate(`/streaming/${banner.albumId}`);
   };
-
+  const checkLive = compareDate(new Date(), new Date(banner.releaseDate)) > 0;
   return (
     <div className="relative rounded-lg" onClick={handleClick}>
       <img
@@ -27,13 +26,19 @@ export function BannerSlide({ banner }: BannerSlideProps) {
         <div className="absolute top-0 left-0 text-grayscale-100 z-10 w-1/4 min-h-full p-4 flex flex-col justify-between select-none cursor-pointer">
           <div>
             <div className="flex flex-row gap-2 text-sm mb-1">
-              <p className="font-bold bg-red-500 px-1">LIVE</p>
-              <p className="text-red-600">{banner.currentUserCount} 명</p>
+              <p
+                className={`font-bold px-1 ${checkLive ? 'bg-red-600' : 'bg-grayscale-400'}`}
+              >
+                LIVE
+              </p>
+              {checkLive && (
+                <p className="text-red-600">{banner.currentUserCount} 명</p>
+              )}
             </div>
             <p className="text-gray-400 break-words">
               #{banner.albumTags?.split(',').join(' #')}
             </p>
-            <p className="font-black text-3xl w-44">{banner.albumName}</p>
+            <p className="font-black text-2xl w-44">{banner.albumName}</p>
           </div>
           <div>
             <p className="font-bold text-2xl">{banner.artist}</p>
